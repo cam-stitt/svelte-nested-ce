337c412d1482c6508731d6fe1973ed258107aeec
Cameron Stitt <cameron.stitt@comparethemarket.com.au>
2019-07-03 13:40:10 +1000

    Initial commit to prove nested slots fails

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<svelte:options tag='my-app' />
+
+<script>
+  import Child from './Child.svelte';
+</script>
+
+<Child>Hello!</Child>
---
+++ src/Child.svelte
@@ -0,0 +1,3 @@
+<svelte:options tag='my-child' />
+
+<h1><slot>A</slot></h1>
